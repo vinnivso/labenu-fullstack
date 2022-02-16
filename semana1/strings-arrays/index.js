1a0238085741aa3f4c012c3836ef22b3f6c57828
vinnivso <vinnivsob@gmail.com>
2022-02-16 09:10:59 -0300

    Update strings-arrays/

--- semana1/strings-arrays/index.js
+++ semana1/strings-arrays/index.js
@@ -1,40 +1,40 @@
 // ------------------ Exercícios de INTERPRETAÇÃO de Código ------------------------
-// 01
-// let array
-// console.log('a. ', array)
-// //RESOLUÇÃO: Somente declarei uma variável e não é dessa maneira que faço uma array, "[]".
+// // 01
+let array
+console.log('a. ', array)
+//RESOLUÇÃO: Somente declarei uma variável e não é dessa maneira que faço uma array, "[]".
 
-// array = null
-// console.log('b. ', array)
-// //RESOLUÇÃO: Estou somente inserindo um valor "null" para a variável declarada como "array".
+array = null
+console.log('b. ', array)
+//RESOLUÇÃO: Estou somente inserindo um valor "null" para a variável declarada como "array".
 
-// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-// console.log('c. ', array.length)
-// //RESOLUÇÃO: Dessa vez, estou declarando fazendo com que a variável "array" se torne uma lista com "n" elementos. Utilizando o ".length" estou solicitando que o console imprima o comprimento dessa lista.
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+//RESOLUÇÃO: Dessa vez, estou declarando fazendo com que a variável "array" se torne uma lista com "n" elementos. Utilizando o ".length" estou solicitando que o console imprima o comprimento dessa lista.
 
-// let i = 0
-// console.log('d. ', array[i])
-// //RESOLUÇÃO: Confuso, mas acredito que, como estamos mencionando a variável array, definida como = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], uma vez que dizemos let i = 0 (Declarando um valor 0 para a variável "i"), porém no console.log definimos "array[i]", ele vai puxar a posição 0 da variável array.
+let i = 0
+console.log('d. ', array[i])
+//RESOLUÇÃO: Confuso, mas acredito que, como estamos mencionando a variável array, definida como = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], uma vez que dizemos let i = 0 (Declarando um valor 0 para a variável "i"), porém no console.log definimos "array[i]", ele vai puxar a posição 0 da variável array.
 
-// array[i+1] = 19
-// console.log('e. ', array)
-// //RESOLUÇÃO: Ao realizarmos "array[i+1]=19", estamos dizendo o mesmo que "array[0+1]=19", portando, estamos passando a instrução para o javascript de que, na seguinte lista [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], atrelado a variável array. No índice [1], assuma um novo valor de 19, trocando seu antigo valor, que era 4.
+array[i+1] = 19
+console.log('e. ', array)
+//RESOLUÇÃO: Ao realizarmos "array[i+1]=19", estamos dizendo o mesmo que "array[0+1]=19", portando, estamos passando a instrução para o javascript de que, na seguinte lista [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], atrelado a variável array. No índice [1], assuma um novo valor de 19, trocando seu antigo valor, que era 4.
 
-// const valor = array[i+6]
-// console.log('f. ', valor)
-// //RESOLUÇÃO: Bem parecido com o anterior, só que nesse caso, estamos chamando uma posição em array. Estamos definindo que valor = índice de um elemento em array. Portanto, estamos afirmando que valor = array[0+6] --> valor = array[6], logo valor = 9.
+const valor = array[i+6]
+console.log('f. ', valor)
+//RESOLUÇÃO: Bem parecido com o anterior, só que nesse caso, estamos chamando uma posição em array. Estamos definindo que valor = índice de um elemento em array. Portanto, estamos afirmando que valor = array[0+6] --> valor = array[6], logo valor = 9.
 
-// 02
-// const frase = prompt("Digite uma frase") //Subi num ônibus em Marrocos
-// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
-// // RESOLUÇÃO: "frase.toUpperCase()", a frase digitada pelo usuário será retornado em caixa alta, logo: "SUBI NUM ÔNIBUS EM MARROCOS". .replaceAll("A", "I")" atrelado ao frase.toUpperCase(), faz com que substitua todos os "A" por "I". frase.length vai me retornar o comprimento daquela frase, lembrando que os espaçamentos também são constados. Sendo assim, a frase vai ficar: "SUBI NUM ÔNIBUS EM MIRROCOS".
+02
+const frase = prompt("Digite uma frase") //Subi num ônibus em Marrocos
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+// RESOLUÇÃO: "frase.toUpperCase()", a frase digitada pelo usuário será retornado em caixa alta, logo: "SUBI NUM ÔNIBUS EM MARROCOS". .replaceAll("A", "I")" atrelado ao frase.toUpperCase(), faz com que substitua todos os "A" por "I". frase.length vai me retornar o comprimento daquela frase, lembrando que os espaçamentos também são constados. Sendo assim, a frase vai ficar: "SUBI NUM ÔNIBUS EM MIRROCOS".
 
 // ------------------ Exercícios de ESCRITA de Código ------------------------------
 //01
 const nome = prompt('Digite seu nome')
 const email = prompt('Digite seu e-mail')
-const frase = `O e-mail ${email} foi cadastrado com sucesso. Seja bem-vindo(a), ${nome}.`
-console.log(frase)
+const fraseFinal = `O e-mail ${email} foi cadastrado com sucesso. Seja bem-vindo(a), ${nome}.`
+console.log(fraseFinal)
 
 //02
 let comidasPreferidas = ['pizza', 'panettone salgado', 'esfiha', 'cajuzinho', 'uva']
@@ -69,6 +69,6 @@ arrayUserPhrase = userPhrase.split(' ')
 console.log(arrayUserPhrase)
 
 //02
-const array = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
-const checkArray = array.indexOf("Abacaxi",0)
-console.log(checkArray, array.length)
+const arrayDeFrutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+const checkArray = arrayDeFrutas.indexOf("Abacaxi", 0)
+console.log(checkArray, arrayDeFrutas.length)
