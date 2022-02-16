aee41d5cf759ac9aacc90df64345232dd3a68fc4
vinnivso <vinnivsob@gmail.com>
2022-02-16 16:04:13 -0300

    Fix: semana1/ strings-arrays/

--- semana1/strings-arrays/index.js
+++ semana1/strings-arrays/index.js
@@ -56,6 +56,7 @@ const userQuestion2 = prompt('Please insert a (2) secondary task, which you have
 const userQuestion3 = prompt('Please insert the (3) last task, which you have to do')
 listaDeTarefas.push(userQuestion1, userQuestion2, userQuestion3)
 // listaDeTarefas = [userQuestion1, userQuestion2, userQuestion3]
+console.log(listaDeTarefas)
 let taskDone = Number(prompt('Dear user, please insert the index of a previously mentioned task that has already been done (1 - 3)'))
 listaDeTarefas.splice((taskDone - 1), 1)
 console.log(listaDeTarefas)
