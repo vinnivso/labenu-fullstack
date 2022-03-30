b553db24f34ea18f9f9cf4352a6c9bc8eb27ef22
Vinícius Oliveira <vinnivsob@gmail.com>
2022-03-30 16:30:02 -0300

    Feat: else in loops

--- modulo1/javascript/semana2/objetos/index.js
+++ modulo1/javascript/semana2/objetos/index.js
@@ -55,123 +55,141 @@ const pessoa = {
 
 console.log(minhaFuncao(pessoa, "backender"))
 //RESOLUÇÃO: Console vai imprimir o valor atribuído para backender.
-console.log(minhaFuncao(pessoa, "altura"))
+// console.log(minhaFuncao(pessoa, "altura"))
 //RESOLUÇÃO: Console vai retornar como undefined ou synthax error, uma vez que não existe a chave altura em pessoa.
 
 // ------------------> EXERCÍCIOS DE ESCRITA DE CÓDIGO <-----------
 // -----------------------> EXERCÍCIO 1 <--------------------------
-const userInput = {
+const personObjectOne = {
   userName: prompt("Please insert your name."),
-  userNickNames: prompt('Please insert your nicknames, separeted by ",".').split(",", 3),
+  userNickNames: prompt(
+    'Please insert your nicknames, separeted by ",".'
+  ).split(",", 3),
 }
 function userPhrase(userObject) {
+  const {userName, userNickNames} = userObject
   console.log(
-    `Eu sou ${userObject.userName}, mas podem me chamar de: ${userObject.userNickNames[0]}, ${userObject.userNickNames[1]} ou ${userObject.userNickNames[2]}.`
+    `Eu sou ${userName}, mas podem me chamar de: ${userNickNames[0]}, ${userNickNames[1]} ou ${userNickNames[2]}.`
   )
-  return userPhrase
 }
-userPhrase(userInput)
-const userInput2 = {
-  ...userInput,
-  userNickNames: prompt('Please insert your nicknames, separed by ",".').split(",",3),
+userPhrase(personObjectOne)
+const personObjectTwo = {
+  ...personObjectOne,
+  userNickNames: prompt('Please insert your nicknames, separed by ",".').split(",",3)
 }
-userPhrase(userInput2)
+userPhrase(personObjectTwo)
 
 // -----------------------> EXERCÍCIO 2 <--------------------------
-const userInputOne = {
-  userName: prompt("Please insert your name."),
-  userAge: Number(prompt("Please insert your age.")),
-  userOccupation: prompt("Please insert your occupation"),
-}
-const userInputTwo = {
-  userName: prompt("Please insert your name"),
-  userAge: Number(prompt("Please insert your age.")),
-  userOccupation: prompt("Please insert your occupation"),
-}
-function userPhrase(userObject) {
-  console.log([
-    userObject.userName,
-    userObject.userName.length,
-    userObject.userAge,
-    userObject.userOccupation,
-    userObject.userOccupation.length,
-  ])
-}
-userPhrase(userInputOne)
-userPhrase(userInputTwo)
+// const userInputOne = {
+//   userName: prompt("Please insert your name."),
+//   userAge: Number(prompt("Please insert your age.")),
+//   userOccupation: prompt("Please insert your occupation"),
+// }
+// const userInputTwo = {
+//   userName: prompt("Please insert your name"),
+//   userAge: Number(prompt("Please insert your age.")),
+//   userOccupation: prompt("Please insert your occupation"),
+// }
+// function userPhrase(userObject) {
+//   const {userName, userAge, userOccupation} = userObject
+//   console.log([
+//     `O valor de nome é: ${userName}`,
+//     `O número de caracteres do valor nome é: ${userName.length}`,
+//     `O valor de idade é: ${userAge}`,
+//     `O valor de profissão é: ${userOccupation}`,
+//     `O número de caracteres do valor profissão é: ${userOccupation.length}`,
+//   ])
+// }
+// userPhrase(userInputOne)
+// userPhrase(userInputTwo)
 
-// -----------------------> EXERCÍCIO 3 <--------------------------
-let carrinho = []
-const objectFruitOne = {
-  userFruit: prompt("Please insert your most favorite fruit."),
-  disponobility: prompt('Do you have this fruit? (Answer only with "yes" or "no".)').includes("yes"),
-}
-const objectFruitTwo = {
-  userFruit: prompt("Please insert your second favorite fruit."),
-  disponobility: prompt('Do you have this fruit? (Answer only with "yes" or "no".)').includes("yes"),
-}
-const objectFruitThree = {
-  userFruit: prompt("Please insert your less favorite fruit."),
-  disponobility: prompt('Do you have this fruit? (Answer only with "yes" or "no".)').includes("yes"),
-}
-function completandandoCarrinho(parameter1, parameter2, parameter3) {
-  return carrinho.push(parameter1, parameter2, parameter3)
-}
-completandandoCarrinho(objectFruitOne, objectFruitTwo, objectFruitThree)
-console.log(carrinho)
+// // -----------------------> EXERCÍCIO 3 <--------------------------
+// let carrinho = []
+// const objectFruitOne = {
+//   userFruit: prompt("Please insert your most favorite fruit."),
+//   disponobility: prompt(
+//     'Do you have this fruit? (Answer only with "yes" or "no".)'
+//   ).includes("yes"),
+// }
+// const objectFruitTwo = {
+//   userFruit: prompt("Please insert your second favorite fruit."),
+//   disponobility: prompt(
+//     'Do you have this fruit? (Answer only with "yes" or "no".)'
+//   ).includes("yes"),
+// }
+// const objectFruitThree = {
+//   userFruit: prompt("Please insert your less favorite fruit."),
+//   disponobility: prompt(
+//     'Do you have this fruit? (Answer only with "yes" or "no".)'
+//   ).includes("yes"),
+// }
+// function completandandoCarrinho(parameter1, parameter2, parameter3) {
+//   carrinho.push(parameter1, parameter2, parameter3)
+// }
+// completandandoCarrinho(objectFruitOne, objectFruitTwo, objectFruitThree)
+// console.log(carrinho)
 
-// ------------------> DESAFIOS <----------------------------------
-// -----------------------> EXERCÍCIO 1 <--------------------------
-function userInput() {
-  const userQuestionName = prompt("Please insert your name.")
-  const userQuestionAge = Number(prompt("Please insert your age."))
-  const userQuestionOccupation = prompt("Please insert your occupation.")
+// // ------------------> DESAFIOS <----------------------------------
+// // -----------------------> EXERCÍCIO 1 <--------------------------
+// function userInputChallenge() {
+//   const userQuestionName = prompt("Please insert your name.")
+//   const userQuestionAge = Number(prompt("Please insert your age."))
+//   const userQuestionOccupation = prompt("Please insert your occupation.")
 
-  const objekt = {
-    userName: userQuestionName,
-    userAge: userQuestionAge,
-    userOccupation: userQuestionOccupation,
-  }
-  console.log(`O nome do usuário é: ${objekt.userName}, possui ${objekt.userAge} anos e sua respectiva ocupação é ${objekt.userOccupation}.`)
-}
-userInput()
+//   const personObjectChallenge = {
+//     userName: userQuestionName,
+//     userAge: userQuestionAge,
+//     userOccupation: userQuestionOccupation,
+//   }
+//   const {userName, userAge, userOccupation} = personObjectChallenge
+//   console.log(
+//     `O nome do usuário é: ${userName}, possui ${userAge} anos e sua respectiva ocupação é ${userOccupation}.`
+//   )
+//   console.log(typeof personObjectChallenge)
+// }
+// userInputChallenge()
 
-// -----------------------> EXERCÍCIO 2 <--------------------------
-function userFilms() {
-  const userFirstMovieName = prompt("Please insert your favorite film.")
-  const userFirstMovieReleaseDate = Number(prompt("Please insert the release date of your favorite film"))
-  const userSecondMovieName = prompt("Please insert your secondary favorite film.")
-  const userSecondMovieReleaseDate = Number(prompt("Please insert the release date of your secondary favorite film."))
-  const objectFilmOne = {
-    FilmName: userFirstMovieName,
-    ReleaseDate: userFirstMovieReleaseDate,
-  }
-  const objectFilmTwo = {
-    FilmName: userSecondMovieName,
-    ReleaseDate: userSecondMovieReleaseDate,
-  }
-  const checkReleaseDateOneByTwo =
-    objectFilmOne.ReleaseDate < objectFilmTwo.ReleaseDate
-  const checkReleaseDateTwoByOne =
-    objectFilmOne.ReleaseDate == objectFilmTwo.ReleaseDate
-  console.log(`O ${objectFilmOne.FilmName} foi lançado antes do ${objectFilmTwo.FilmName}?: ${checkReleaseDateOneByTwo}.
-    O ${objectFilmOne.FilmName} foi lançado no mesmo ano do ${objectFilmTwo.FilmName}?: ${checkReleaseDateTwoByOne}.`)
-}
-userFilms()
+// // -----------------------> EXERCÍCIO 2 <--------------------------
+// const matrix = {
+//   nome: "Matrix",
+//   anoDeLancamento: 1999
+// };
 
-// -----------------------> EXERCÍCIO 3 <--------------------------
+// const johnWick = {
+//   nome: "John Wick",
+//   anoDeLancamento: 2014
+// };
+
+// function comparaAnoDeLancamento(primeiroFilme, segundoFilme) {
+//   return `O primeiro filme foi lançado antes do segundo? ${
+//     primeiroFilme.anoDeLancamento < segundoFilme.anoDeLancamento
+//   }
+// O primeiro filme foi lançado no mesmo ano do segundo? ${
+//     primeiroFilme.anoDeLancamento === segundoFilme.anoDeLancamento
+//   }`;
+// }
+
+// console.log(comparaAnoDeLancamento(matrix, johnWick));
+
+// // -----------------------> EXERCÍCIO 3 <--------------------------
 let carrinhoChallenge = []
 const objectFruitOneChallenge = {
   userFruit: prompt("Please insert your most favorite fruit."),
-  disponobility: prompt('Do you have this fruit? (Answer only with "yes" or "no".)').includes("yes"),
+  disponobility: prompt(
+    'Do you have this fruit? (Answer only with "yes" or "no".)'
+  ).includes("yes"),
 }
 const objectFruitTwoChallenge = {
   userFruit: prompt("Please insert your second favorite fruit."),
-  disponobility: prompt('Do you have this fruit? (Answer only with "yes" or "no".)').includes("yes"),
+  disponobility: prompt(
+    'Do you have this fruit? (Answer only with "yes" or "no".)'
+  ).includes("yes"),
 }
 const objectFruitThreeChallenge = {
   userFruit: prompt("Please insert your less favorite fruit."),
-  disponobility: prompt('Do you have this fruit? (Answer only with "yes" or "no".)').includes("yes"),
+  disponobility: prompt(
+    'Do you have this fruit? (Answer only with "yes" or "no".)'
+  ).includes("yes"),
 }
 function completandandoCarrinho(parameterOne, parameterTwo, parameterThree) {
   return carrinho.push(parameterOne, parameterTwo, parameterThree)
